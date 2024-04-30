6496c5d2fb846fca3916c3145e48adce1ced77de
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-04-30 16:14:09 +0000

    correct a typo in settings.py

--- profiles/models.py
+++ profiles/models.py
@@ -10,7 +10,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to="images/", default="../default-profile_r5dhks"
+        upload_to="images/", default="../default_profile_ztfnuk"
     )
     profession = models.CharField(max_length=255, blank=True)
     lives_in = models.CharField(max_length=255, blank=True)
