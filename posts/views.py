a1bb6de80f46fdde5759a80bc69292599c46ac07
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-05-02 17:40:52 +0000

    correct typo

--- posts/views.py
+++ posts/views.py
@@ -65,7 +65,9 @@ class PostDetail(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+            )
 
     def delete(self, request, pk):
         post = self.get_object(pk)
